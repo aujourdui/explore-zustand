4ca7e0b3753ecf07793f66b9f3161b553e9ec67e
ito <kyosuke.ito@gmail.com>
2022-05-08 20:22:16 -0700

    create a basic zustand counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
+import create from "zustand";
+
+const useStore = create((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  reset: () => set({ count: 0 }),
+}));
 
 function App() {
+  const count = useStore((state) => state.count);
+  const increment = useStore((state) => state.increment);
+  const decrement = useStore((state) => state.decrement);
+  const reset = useStore((state) => state.reset);
+
   return (
     <div className="App">
       <h1>Zustand</h1>
+      <div>{count}</div>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
